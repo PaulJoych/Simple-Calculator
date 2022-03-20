2756d27a9ba124bdd46bc1fe5824787d700b153c
142Eko <ekorahmatsatrio@gmal.com>
2022-03-20 15:13:43 +0700

    142Eko

--- main.cpp
+++ main.cpp
@@ -1,73 +1,94 @@
 #include <iostream>
 using namespace std;
-
+ 
 int calculation(char op, float num1, float num2) {
   float result;
-  bool err = false;
-
+ 
   switch(op) {
-
     case '+':
       result = num1 + num2;
       break;
-
+ 
     case '-':
       result = num1 - num2;
       break;
-
+ 
     case '*':
       result = num1 * num2;
       break;
-
+ 
     case '/':
       result = num1 / num2;
       break;
-
-    default:
-      err = true;
-      break;
   }
-
-  if (err) {
-    cout << "--------------------------------  \n";
-    cout << "Error! operator is not correct \n";
-  } else {
-    cout << "--------------------------------  \n";
-    cout << num1 << " " << op << " " << num2 << " = " << result << " \n";
-  }
-
+ 
+  cout << "--------------------------------  \n";
+  cout << num1 << " " << op << " " << num2 << " = " << result << " \n";
+ 
   return 0;
 }
-
+ 
 int main() {
   char op;
   float num1, num2;
-
-  cout << "--------------------------------  \n";
-  cout << "Enter your operater: +, -, *, /:  \n";
+  bool opChecker;
+ 
+  revert:
+ 
+  opChecker = false;
+ 
+  // op getter
+  cout << "Masukkan operator : +, -, *, /:  \n";
   cin >> op;
-
+ 
+  switch(op) {
+    case '+':
+      break;
+ 
+    case '-':
+      break;
+ 
+    case '*':
+      break;
+ 
+    case '/':
+      break;
+ 
+    default:
+      opChecker = true;
+      break;
+  }
+ 
+  if (opChecker == true) {
+    cout << "--------------------------------  \n";
+    cout << "Error! operator harus  +, -, *, /" << endl;
+    goto revert;
+  }
+ 
+  // float getter
   cout << "--------------------------------  \n";
-  cout << "Enter your first calculation:  \n";
+  cout << "Masukkan angka pertama :  \n";
   cin >> num1;
-
+ 
   while(cin.fail()) {
-    cout << "Error! value first must be number \nEnter your first calculation:" << endl;
+    cout << "--------------------------------  \n";
+    cout << "Error! nilai pertama harus angka \nMasukkan angka pertama:" << endl;
     cin.clear();
     cin.ignore(256,'\n');
     cin >> num1;
   }
-
+ 
   cout << "--------------------------------  \n";
-  cout <<  "Enter your second calculation:  \n";
+  cout <<  "Masukkan angka kedua:  \n";
   cin >> num2;
-
+ 
   while(cin.fail()) {
-    cout << "Error! value second must be number \nEnter your second calculation:" << endl;
+    cout << "--------------------------------  \n";
+    cout << "Error! nilai kedua harus kedua \nMasukkan angka kedua:" << endl;
     cin.clear();
     cin.ignore(256,'\n');
     cin >> num2;
   }
-
+ 
   return calculation(op, num1, num2);
 }
